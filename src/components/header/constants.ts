c09e07a1f418480b699de51739f9ccff185b7cce
Augusto-DF <neoaugustocode@gmail.com>
2024-08-27 18:21:53 -0300

    feat: Add header component

---
+++ src/components/header/constants.ts
@@ -0,0 +1,10 @@
+export {}
+
+const HEADER_DATA = [
+    { label: "Home", href: "/" },
+    { label: "About", href: "" },
+    { label: "Projects", href: "" },
+    { label: "Contact", href: "#contact" },
+]
+
+export { HEADER_DATA }
