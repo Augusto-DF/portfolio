22319d175de3e57809f45a8dd29e9d71fa026d75
Augusto-DF <neoaugustocode@gmail.com>
2024-09-09 16:36:43 -0300

    feat: Create project card component

---
+++ src/components/project-card/index.tsx
@@ -0,0 +1,33 @@
+import { flagsKeys } from '@components/flag-tag/constants'
+import TierTag, { TierTagProps } from '@components/tier-tag'
+
+import styles from './styles.module.scss'
+import FlagTag from '@components/flag-tag'
+import { Link } from 'react-router-dom'
+
+export type ProjectCardProps = {
+  title: string
+  country: (typeof flagsKeys)[number]
+  tags: Array<TierTagProps>
+  href: string
+}
+
+const ProjectCard = ({ title, country, tags, href }: ProjectCardProps) => {
+  return (
+    <Link to={href}>
+      <div className={styles.cardContainer}>
+        <div className={styles.imageContainer}></div>
+        <h2>{title}</h2>
+        <FlagTag countryFlag={country} />
+        <div className={styles.tagsContainer}>
+          {tags.map((tag, id) => {
+            const { label } = tag
+            return <TierTag key={`${label}-project-tag-${id}`} {...tag} />
+          })}
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default ProjectCard
