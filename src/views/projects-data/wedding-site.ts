953b6236f09982771bcdf0e887dfc52f631838a8
Augusto-DF <neoaugustocode@gmail.com>
2024-10-01 16:01:42 -0300

    feat: Add wedding page data

---
+++ src/views/projects-data/wedding-site.ts
@@ -0,0 +1,62 @@
+export {}
+
+export const data = {
+    tags: [{label: 'Landing page', tier: "mythic"}, {label: 'React', tier: "legend"}, {label: 'Express', tier: "ultimate"}, {label: 'Typescript', tier: "rare"},],
+    country: 'brazil',
+    company: 'Freelance',
+    year: ['2024'],
+    title: 'Nature themed wedding',
+    hero_image: '',
+    hero_img_alt: '',
+    about: {
+        description: 'Our clients wanted a minimalist wedding website that include sections on outfits, venue, date, and gift list.',
+        client: 'Freelancer',
+        start:{ year: '2024', month: 'Jun' },
+        end:{ year: '2024', month: 'Jul' },
+        platform: 'Desktop/Mobile (Responsive)',
+        role: 'Front-end & back-end developer, Dev Ops',
+        tools: [{label: '', icon: ''}]
+    },
+    my_role: {        
+        description: 'I was hired as a full-stack developer and was responsible for:',
+        list: [
+            'Collaborating with the designer', 
+            'Defined the technical feasibility', 
+            'Chose the technologies', 
+            "Suggest alternative approaches to meet our client's needs"
+        ]        
+    },
+    challenge: {
+        description: 'The client had an initial design of the product but there where many elements missing. I was responsible for:',
+        list: [
+            "Implementing the count down",
+            "Build the gift list",
+            "Components creation",
+        ]
+    },
+    acomplishments: {
+        list: [
+            "Implementing the count down",
+            "Build the gift list",
+            "Components creation",
+        ],
+        image: ""
+    },
+    learning: {
+        list: [
+            {
+                title: "Creating and managing design system at scale",
+                description: "Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+            },
+            {
+                title: "Balancing pace and perfection",
+                description: "Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+            },
+            {
+                title: "Receiving feedback and iterating",
+                description: "Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+            }
+        ],
+        image: ''
+    }
+}
