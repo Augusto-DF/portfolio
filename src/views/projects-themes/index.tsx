8f13f0da4e914ff12163493f19d760af46e6c8fc
Augusto-DF <neoaugustocode@gmail.com>
2024-10-01 16:02:45 -0300

    feat: Create Themes views to manage the possible themes so show the projects

---
+++ src/views/projects-themes/index.tsx
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router-dom'
+import { data as wedding } from '../projects-data/wedding-site'
+import ThemeA from './theme-a'
+import { useMemo } from 'react'
+
+const Themes = () => {
+  const { targetProject: project } = useParams()
+  console.log('targetProject', project)
+
+  const data = useMemo(() => {
+    switch (project) {
+      case 'wedding':
+        return wedding
+      case 'vetted':
+        return wedding
+      default:
+        return
+    }
+  }, [project])
+
+  /* TODO: When the BE was sending the projects' data make a function that select the theme randomly or select the theme based on some information sent by BE */
+
+  return <ThemeA data={data} />
+}
+
+export default Themes
