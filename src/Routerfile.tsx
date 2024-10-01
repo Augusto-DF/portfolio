ac68a27819c52d05104664119df0fc2b33c56190
Augusto-DF <neoaugustocode@gmail.com>
2024-10-01 15:57:39 -0300

    feat: Add  route

--- src/Routerfile.tsx
+++ src/Routerfile.tsx
@@ -2,6 +2,7 @@ import Footer from '@components/footer'
 import Header from '@components/header'
 import Home from '@views/home'
 import Projects from '@views/projects'
+import Themes from '@views/projects-themes'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 const Routerfile = () => {
@@ -11,6 +12,7 @@ const Routerfile = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:targetProject" element={<Themes />} />
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
       <Footer />
