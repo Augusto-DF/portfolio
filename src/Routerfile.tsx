0b3a41e5df8eac23724e0dbfb84e236df0582bbc
Augusto-DF <neoaugustocode@gmail.com>
2025-11-05 19:56:16 -0300

    feat: Add test-area path

--- src/Routerfile.tsx
+++ src/Routerfile.tsx
@@ -3,6 +3,7 @@ import Header from '@components/header'
 import Home from '@views/home'
 import Projects from '@views/projects'
 import Themes from '@views/projects-themes'
+import TestArea from '@views/test-area'
 import { useEffect } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 
@@ -19,6 +20,7 @@ const Routerfile = () => {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/:targetProject" element={<Themes />} />
+        <Route path="/test-area" element={<TestArea />} />
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
       <Footer />
