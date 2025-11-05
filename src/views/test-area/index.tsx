746b5353cc4e6bc28b49b66ecf20552f4463f0c7
Augusto-DF <neoaugustocode@gmail.com>
2025-11-04 21:16:26 -0300

    feat: Add button animation

--- src/views/test-area/index.tsx
+++ src/views/test-area/index.tsx
@@ -1,31 +1,80 @@
-import Arise from "@components/animated/arise"
+import { animate } from 'motion'
+import * as motion from 'motion/react-client'
 
 const TestArea = () => {
   return (
-    <div style={style}>
-        <Arise><div style={squireTarget()} /></ Arise>
-        <div style={squireTarget("#6f85c1ff")}></div>
-    </div>
+    <>
+      {/* <div style={{ width: '100%', height: '100vh', backgroundColor: '#000' }}></div> */}
+      <div style={style}>
+        <motion.div
+          style={squireTarget()}
+          animate={{
+            scale: [null, 0.2, 4, 1],
+            transition: {
+              duration: 3,
+              times: [0, 0.3, 0.4, 1],
+              //repeat: Infinity,
+              //repeatDelay: 1,
+              ease: ['easeInOut', 'easeOut'],
+            },
+          }}
+        />
+
+        <motion.button
+          whileHover={{ boxShadow: '3px 3px 0px 0px #fff', borderColor: '#fff', x: -3, y: -3 }}
+          transition={{ duration: 0.1 }}
+          style={buttonStyle}
+        >
+          test
+        </motion.button>
+
+        <motion.div
+          style={squireTarget('#6f85c1ff')}
+          //initial={}
+          animate={{
+            transition: {
+              duration: 1,
+              repeat: Infinity,
+              repeatDelay: 1,
+              ease: 'easeInOut',
+              //times: [0.5, 0.5],
+            },
+          }}
+        />
+      </div>
+    </>
   )
 }
 
-const squireTarget = (color = "#d089dcff") => ({
-    width: "65px",
-    height: "65px",
-    backgroundColor: color,
-    borderRadius: "8px"
+const squireTarget = (color = '#d089dcff') => ({
+  width: '65px',
+  height: '65px',
+  backgroundColor: color,
+  borderRadius: '8px',
 })
 
 // #d4476ff
 
 const style = {
-    width: "100vw",
-    height: "80vh",
-    backgroundColor: "#e0e0e0",
-    display: 'flex',
-    justifyContent: "center",
-    alignItems: "center",
-    gap: "16px"
+  width: '100vw',
+  height: '80vh',
+  backgroundColor: '#282930', //'#e0e0e0',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '16px',
+}
+
+const buttonStyle = {
+  padding: '0.6rem 1.8rem',
+  fontFamily: 'Montserrat',
+  fontSize: '1.4rem',
+  cursor: 'pointer',
+  fontWeight: 600,
+  border: '0.1rem solid transparent',
+  borderRadius: '0.8rem',
+  color: '#fff',
+  backgroundColor: '#282930',
 }
 
 export default TestArea
