a5495c0fbf9e71b54ac99560e2b51c3cde483926
Augusto-DF <neoaugustocode@gmail.com>
2025-11-18 02:30:28 +0100

    enhancement: Make the Moveins in an array triggers separatly

--- src/components/animated/move-in/index.tsx
+++ src/components/animated/move-in/index.tsx
@@ -1,30 +1,63 @@
 import { ChildrenType } from '@components/utils'
 import * as motion from 'motion/react-client'
+import { useId } from 'react'
 
-type MoveInType = {
-  children: Array<ChildrenType> | ChildrenType
-  delay?: number
+type DirectionType = 'right' | 'left' | 'up' | 'down'
+
+type MoveInType<T extends readonly ChildrenType[] = readonly ChildrenType[]> = {
+  children: T | ChildrenType
+  direction?: DirectionType | { [K in keyof T]: DirectionType }
+  className?: string
 }
 
-const MoveIn = ({ children, delay }: MoveInType) => {
+const MoveIn = ({ children, direction = 'left', className }: MoveInType) => {
+  const id = useId()
   const childrenList = Array.isArray(children) ? children : new Array(children)
-  return (
-    <motion.div
-      initial={{
-        x: -80,
-        opacity: 0,
-      }}
-      whileInView={{
-        x: 0,
-        opacity: 1,
-        transition: {
-          duration: 0.7,
-        },
-      }}
-    >
-      {children}
-    </motion.div>
-  )
+
+  const renderChildren = () => {
+    const getOffset = (dir: DirectionType | readonly DirectionType[]) => {
+      switch (dir) {
+        case 'left':
+          return { x: -80, y: 0 }
+        case 'right':
+          return { x: 80, y: 0 }
+        case 'up':
+          return { x: 0, y: 80 }
+        case 'down':
+          return { x: 0, y: -80 }
+        default:
+          return { x: 0, y: 0 }
+      }
+    }
+
+    return childrenList.map((child, index) => {
+      const dir = Array.isArray(direction) ? direction[index] : direction
+      const offset = getOffset(dir)
+      return (
+        <motion.div
+          className={className}
+          key={`moveIn-${id}-${index}`}
+          initial={{
+            ...offset,
+            opacity: 0,
+          }}
+          whileInView={{
+            x: 0,
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 0.7,
+            },
+          }}
+          viewport={{ margin: '-300px', once: true }}
+        >
+          {child}
+        </motion.div>
+      )
+    })
+  }
+
+  return <>{renderChildren()}</>
 }
 
 export default MoveIn
